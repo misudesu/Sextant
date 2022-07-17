e9ba8f07c78a520718dde9ae142a6e69b954bd9c
misudesu <misaeldessalegn@gmail.com>
2022-07-17 21:07:12 +0300

    commit

--- src/component/Exhibit.js
+++ src/component/Exhibit.js
@@ -1,11 +1,17 @@
 import '../App.css';
 import {BrowserRouter as Router,Route,Routes,Link,useLocation,Nav} from 'react-router-dom'
 function Exhibit(){
+    const ips="2001:db8:3333:4444:5555:6666:7777:8888";
+    const ipss="192.168.10.100";
     return(
         <div>
 <header id="App-header">
-<Link  to="/" >Home</Link>
-<p> <Link  to="/About" >About</Link>
+<Link  to={`/${ips}`} state={{ 
+  ids:ips,
+}} >iPv6</Link>
+<p> <Link    to={`/${ipss}`} state={{ 
+  idss:ipss,
+}}  >iPv4</Link>
     </p>
     <hr/>
 </header>
--- src/component/Home.js
+++ src/component/Home.js
@@ -1,8 +1,64 @@
-function Home(){
-    return(
-<div>
-<p>this is Home page </p>
-</div>
-    );
+import React,{useState,useEffect} from 'react'
+import {BrowserRouter as Router,Route,Routes,Link,useParams,useLocation} from 'react-router-dom'
+
+export default function Home(props){
+    const {idss}=useLocation().state;
+    const {ips}=useParams(); 
+    const [Ip,setIp]=useState(null);
+    const [checkip,setCheckip]=useState(null)
+  //"2001:db8:3333:4444:5555:6666:7777:8888"
+  //checkIfValidIP(ips); // true
+  // checkIfValidIP("192.168.1.1"); // false   
+     
+useEffect(()=>{
+   
+    if(checkIfValidIP(ips)===true){
+        fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_6gKfJkuMqSnvSvRgY5YdDSf5heOHi&ipAddress=${ips}`,{
+        method:'GET',
+        headers:{},
+    }
+        ).then(res=>{
+           // console.log(res);
+            return res.text()
+        }).then(location=>{
+            console.log(location);
+            setIp(location);
+        })   
+    }else{
+        fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_6gKfJkuMqSnvSvRgY5YdDSf5heOHi&ipAddress=${idss}`,{
+            method:'GET',
+            headers:{},
+        }
+            ).then(res=>{
+               // console.log(res);
+                return res.text()
+            }).then(location=>{
+                console.log(location);
+                setIp(location);
+            }) 
+    }
+     
+},[ips])
+
+function checkIfValidIP(str) {
+    // Regular expression to check if string is a IPv6 address
+    const regexExp = /(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))/gi;
+   /// setCheckip(regexExp.test(str));       
+    return regexExp.test(str);
+  }
+if(Ip){
+  return(  <div>
+    
+    <p> your ip iS  iPv6 </p>
+    {Ip} 
+    </div>  
+  );
 }
-export default Home;
+ return(
+<div>
+<p>your ip iS  iPv4. </p>
+<hr/>
+{Ip}
+</div>
+ )  ; 
+}
